45e0b431520ccb236338ecc9be77eab689cbb5dc
Alexander.D <74902666+lonegdd@users.noreply.github.com>
2023-03-10 00:44:34 +0300

    style skills

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -3,6 +3,7 @@ import style from "./Skills.module.css"
 import StyleContainer from "../common/styles/Container.module.css"
 import {Skill} from "./skill/Skill";
 import {Title} from "../common/components/title/Title";
+import {faJs, faCss3Alt,faReact} from '@fortawesome/free-brands-svg-icons';
 
 export const Skills = () => {
     return (
@@ -10,9 +11,9 @@ export const Skills = () => {
             <div className={`${StyleContainer.container} ${style.skillsContainer}`}>
                <Title title={"My Skills"} titleSpan={"Skills"}/>
                 <div className={style.skills}>
-                    <Skill title={'JS'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}/>
-                    <Skill title={'CSS'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'}/>
-                    <Skill title={'REACT'} description={'It has survived not only five centuries'}/>
+                    <Skill FontAwesomeIcon={faJs} title={'JS'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'}/>
+                    <Skill FontAwesomeIcon={faCss3Alt} title={'CSS'} description={'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s'}/>
+                    <Skill FontAwesomeIcon={faReact} title={'REACT'} description={'It has survived not only five centuries'}/>
                 </div>
             </div>
         </div>
--- src/skills/skill/Skill.js
+++ src/skills/skill/Skill.js
@@ -1,11 +1,15 @@
 import React from "react";
 import style from "./Skill.module.css"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 
 export const Skill = (props) => {
     return (
         <div className={style.skill}>
-            <div className={style.icon}></div>
+           <div className={style.iconBox}>
+               <FontAwesomeIcon icon={props.FontAwesomeIcon} className={style.icon} />
+           </div>
+
             <h2 className={style.projectTitle}>{props.title}</h2>
             <span className={style.description}>{props.description}</span>
         </div>
